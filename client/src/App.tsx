e117036463cd7eb12693344ee607c06289f454c6
Nidhish-D-Nayak <nidhibelthangady@gmail.com>
2023-11-28 00:27:17 +0530

    Added both login and register page, initiating react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import Login from "./pages/login/Login.tsx";
+import Register from "./pages/register/Register.tsx";
 
 const App = () => {
 	return (
 		<div>
 			<Login />
+			<Register />
 		</div>
 	);
 };
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,7 +1,41 @@
 import "./register.scss";
 
 const Register = () => {
-	return <div className="register">Register</div>;
+	return (
+		<div className="register">
+			<div className="card">
+				<div className="left">
+					<div className="left-heading">
+						<h1>LinkX.</h1>
+						<p>
+							Welcome to our vibrant social media community, where meaningful
+							connections thrive! Immerse yourself in a world of shared
+							experiences, as our platform seamlessly brings together like
+							minded individuals. Join us on a journey of authentic expression
+							and community engagement.
+						</p>
+					</div>
+					<div className="left-register">
+						<span>Already have an account ?</span>
+						<button>Login</button>
+					</div>
+				</div>
+				<div className="right">
+					<div>
+						<h2>Don't have an account ?</h2>
+						<p>Register with your email and password</p>
+					</div>
+					<form action="">
+						<input type="text" placeholder="Username" />
+						<input type="email" placeholder="Email" />
+						<input type="password" placeholder="Password" />
+						<input type="text" placeholder="Name" />
+						<button>Register</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Register;
