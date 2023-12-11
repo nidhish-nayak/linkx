9147854f40ff2593358921b056532283f6b04492
nidhish-nayak <nidhibelthangady@gmail.com>
2023-12-12 00:36:46 +0530

    Added style.scss for intializing dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-import "./App.css";
 import Leftbar from "./components/leftbar/leftbar";
 import Navbar from "./components/navbar/navbar";
 import Rightbar from "./components/rightbar/rightbar";
+import "./style.scss";
 
 const App = () => {
 	return (
-		<div className="app">
+		<div className="theme-dark">
 			<Navbar />
 			<div style={{ display: "flex" }}>
 				<Leftbar />
