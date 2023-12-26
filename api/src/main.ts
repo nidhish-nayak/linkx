d4c7f36a126f76c087d1544a35af19283c1513c7
nidhish-nayak <nidhibelthangady@gmail.com>
2023-12-26 19:50:15 +0530

    NodeJS setup complete with typescript, zod and eslint

---
+++ api/src/main.ts
@@ -0,0 +1,20 @@
+import express, {
+    type Application,
+    type Request,
+    type Response,
+} from "express";
+import { zodMiddleware } from "./middlewares/zod.middleware";
+
+const app: Application = express();
+
+app.use(zodMiddleware);
+app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => {
+    res.send("Hello World!");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
