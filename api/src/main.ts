952e98271d231ca9cd841c1705cda8cc88e7b770
nidhish-nayak <nidhibelthangady@gmail.com>
2023-12-28 13:51:03 +0530

    Added comments to main.ts, moved app.use(zodMiddleware) after handlers

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,13 +7,17 @@ import { zodMiddleware } from "./middlewares/zod.middleware";
 
 const app: Application = express();
 
-app.use(zodMiddleware);
 app.use(express.json());
 
+// Routes
 app.get("/", (_req: Request, res: Response) => {
     res.send("Hello World!");
 });
 
+// Global catches using zod
+app.use(zodMiddleware);
+
+// Listener
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log("Server is running on port: " + `http://localhost:${PORT}`);
