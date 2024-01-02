327f4f903676e9ccc38dc2226d6a14f342d0f98c
nidhish-nayak <nidhibelthangady@gmail.com>
2024-01-02 14:14:01 +0530

    Switching from db connect to Pool - optimized with types

--- api/src/db/connect.ts
+++ api/src/db/connect.ts
@@ -1,8 +1,18 @@
-import mysql from "mysql2";
+import mysql, { type Pool, type PoolConnection } from "mysql2/promise";
 
-export const db = mysql.createConnection({
+const dbPool: Pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 });
+
+export async function executeQuery<L extends string, Q>(query: L, params: Q) {
+    const connection: PoolConnection = await dbPool.getConnection();
+    try {
+        const [results] = await connection.query(query, params);
+        return results;
+    } finally {
+        connection.release();
+    }
+}
